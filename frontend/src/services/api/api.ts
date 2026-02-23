8406d82fec99c39884eabfc0aee59b6674b27927
srgsuh <srg.suhoverhov@gmail.com>
2026-02-23 14:47:55 +0200

    Standardize env variable names, add check for base URL parameter, minor logging fixes

--- frontend/src/services/api/api.ts
+++ frontend/src/services/api/api.ts
@@ -7,33 +7,37 @@ const logger = getLogger("ApiClient");
 
 const DEFAULT_TIMEOUT: number = 3000;
 
-const API_BASE_URL: string = config.apiBaseUrl ?? '';
-const API_TIME_OUT: number = config.apiTimeout ?? DEFAULT_TIMEOUT;
+const API_BASE_URL: string = config.apiBaseUrl;
+const API_TIMEOUT: number = config.apiTimeout ?? DEFAULT_TIMEOUT;
 
 class ApiClient {
     private readonly client: AxiosInstance;
     
-    constructor(baseUrl: string, timeout: number) {    
+    constructor(baseUrl: string, timeout: number) {
+        if (!baseUrl) {
+            throw new Error('API base URL is not configured');
+        }
         this.client = axios.create({
             baseURL: baseUrl,
             timeout: timeout,
             headers: {
                 'Content-Type': 'application/json'
             },
-        })
+        });
+        logger.debug(`Created API client. Params: url=${baseUrl}, timeout=${timeout}`);
     }
 
     async get<T>(
         endpoint: string,
         config?: AxiosRequestConfig
     ): Promise<T> {
-        logger.debug('GET request to', endpoint, 'with config:', config);
+        logger.debug('GET request', { endpoint, params: config?.params });
         const response: AxiosResponse<T> = await this.client.get<T>(
             endpoint,
             {...config},
         );
-        logger.debug(`GET Response status = ${response.status}, `);
-        logger.debug(`GET Response data = ${response.data}`);
+        logger.debug(`GET Response status: ${response.status}, `);
+        logger.debug(`GET Response data:`, response.data);
         return response.data;
     }
 
@@ -43,19 +47,19 @@ class ApiClient {
         config?: AxiosRequestConfig
     ): Promise<T> {
         logger.debug('POST request to', endpoint, 'with config:', config);
-        logger.debug(`POST data: ${data}`);
+        logger.debug(`POST request data: `, data);
         const response = await this.client.post<T>(
             endpoint,
             data,
             { ...config },
         );
-        logger.debug(`POST Response status = ${response.status}, `);
-        logger.debug(`POST Response data = ${response.data}`);
+        logger.debug(`POST Response status: ${response.status}, `);
+        logger.debug(`POST Response data:`, response.data);
         return response.data;
     }
 }
 
 export const apiClient: ApiClient = new ApiClient(
     API_BASE_URL,
-    API_TIME_OUT,
+    API_TIMEOUT,
 );
