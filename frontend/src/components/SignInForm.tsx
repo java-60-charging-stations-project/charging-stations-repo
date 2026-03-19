066c896d0e52be4476408d119211957446941b5d
srgsuh <srg.suhoverhov@gmail.com>
2026-03-19 12:27:53 +0200

    Add basic layout css styles

--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -1,5 +1,5 @@
 import { useState, type FC } from "react";
-import { useNavigate } from "react-router";
+import SimpleButton from "./SimpleButton";
 
 interface SignInFormProps{
   isRegister: boolean;
@@ -17,8 +17,6 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
   const [confirmPassword, setConfirmPassword] = useState<string | undefined>(undefined);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  
-  const navigate = useNavigate();
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -26,26 +24,12 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
     submitHandler(email, password, confirmPassword, combinedName);
   };
 
-  const handleConfirmExistingAccount = () => {
-    const trimmedEmail = email.trim();
-    navigate("/confirm", {
-      state: {
-        email: trimmedEmail || undefined,
-      },
-    });
-  };
-
   return (
-  <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen w-full">
-      <h1>{isRegister?"SIGN UP PAGE": "LOGIN PAGE"}</h1>
-      <h4>
-          {isRegister ? "Sign up to create an account" : "Sign in to your account"}
-      </h4>
-    <form onSubmit={ handleSubmit }>
-      <div>
-        <label className="block text-gray-700 font-medium mb-1">Email</label>
+    <>
+      <h1>{isRegister ? "SIGN UP PAGE" : "LOGIN PAGE"}</h1>
+      <h4>{isRegister ? "Sign up to create an account" : "Sign in to your account"}</h4>
+      <form onSubmit={handleSubmit} className="w-full flex flex-col space-y-3">
         <input
-          className="inputText"
           id="email"
           type="email"
           value={email}
@@ -53,12 +37,8 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
           placeholder="Email"
           required
         />
-      </div>
-      {isRegister && (
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">First Name</label>
+        {isRegister && (
           <input
-            className="inputText"
             id="firstName"
             type="text"
             value={firstName}
@@ -66,12 +46,9 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
             placeholder="First Name"
             required
           />
-        </div>
-      )}
-      {isRegister && (
-        <div>
+        )}
+        {isRegister && (
           <input
-            className="inputText"
             id="lastName"
             type="text"
             value={lastName}
@@ -79,11 +56,8 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
             placeholder="Last Name"
             required
           />
-        </div>
-      )}
-      <div>
+        )}
         <input
-          className="inputText"
           id="password"
           type="password"
           value={password}
@@ -91,11 +65,8 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
           placeholder="Password"
           required
         />
-      </div>
-      {isRegister && (
-        <div>
+        {isRegister && (
           <input
-            className="inputText"
             id="confirmPassword"
             type="password"
             value={confirmPassword}
@@ -103,19 +74,15 @@ const SignInForm: FC<SignInFormProps> = ({isRegister, submitHandler}) => {
             placeholder="Confirm Password"
             required
           />
-        </div>
-      )}
-      <button type="submit">{isRegister ? "Sign Up" : "Sign In"}</button>
-    </form>
-    <button type="button" onClick={handleConfirmExistingAccount}>
-      Confirm existing account
-    </button>
-    <button type="button" onClick={isRegister? () => navigate("/login"): () => navigate("/register")}>
-      {isRegister
-        ? "Already have an account? Sign In"
-        : "Need an account? Sign Up"}
-      </button>
-  </div>
+        )}
+        <SimpleButton
+          buttonType="submit"
+          caption={isRegister ? "Sign Up" : "Sign In"}
+          color="primary"
+          className="w-full"
+        />
+      </form>
+    </>
   );
 };
 
