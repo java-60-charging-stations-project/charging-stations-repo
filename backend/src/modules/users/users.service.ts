18b59da760d305d104aa784125787c74bf59e5c1
srgsuh <srg.suhoverhov@gmail.com>
2026-03-22 15:55:49 +0200

    Change local backend to use cognito local service

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,11 +1,11 @@
 import { env } from "../../config/env";
 import { UsersServiceLambda } from "./users.service.lambda";
-import { UsersServiceLocal } from "./users.service.local";
+import { UsersServiceAwsLocal } from "./users.service.awslocal";
 import { UsersService } from "./users.types";
 
 export function buildUsersService(): UsersService {
     if (env.environment === 'local') {
-        return new UsersServiceLocal();
+        return new UsersServiceAwsLocal();
     }
     return new UsersServiceLambda();
 }  
