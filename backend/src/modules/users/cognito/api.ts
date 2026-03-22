a623e48575b2a239a781fa90f8c4fc2979f32286
srgsuh <srg.suhoverhov@gmail.com>
2026-03-22 11:57:11 +0200

    Implement basic methods for the local AWS service

--- backend/src/modules/users/cognito/api.ts
+++ backend/src/modules/users/cognito/api.ts
@@ -105,6 +105,7 @@ export class CognitoUsersAPI {
             UserPoolId: this.userPoolId,
         });
         return this.client.send(command);
+
     }
 }
 
--- backend/src/modules/users/users.service.awslocal.ts
+++ backend/src/modules/users/users.service.awslocal.ts
@@ -1,3 +1,4 @@
+import { AdminGetUserResponse } from '@aws-sdk/client-cognito-identity-provider';
 import {
     AdminUserDetails,
     GetUserDetailsFilters,
@@ -10,29 +11,63 @@ import {
     UserRole,
     UsersService,
 } from './users.types';
+import { ResourceNotFoundError, InternalServerError } from '../../common/serviceErrors';
+import { cognitoApiClient } from './cognito/api';
+import { getUserInfoFromCognitoUser,
+    unpackAdminGetUserResponse,
+    unpackListUsersResponse } from './cognito/utils';
+import { createLogger } from '../../utils/logger';
+import { CognitoUser } from './cognito/types';
+
+const logger = createLogger('UsersServiceAwsLocal');
+
+function throwUserNotFoundError(userId: string): never {
+    throw new ResourceNotFoundError(`User ${userId} not found`, 'USER_NOT_FOUND');
+}
 
 export class UsersServiceAwsLocal implements UsersService {
+
     async getMyInfo(_userId: string): Promise<UserInfo> {
-        throw new Error('Not implemented');
+        logger.debug('Getting my info: ', { _userId });
+        const cognitoResponse: AdminGetUserResponse = await cognitoApiClient.getUserDetails(_userId);
+        if (!cognitoResponse) {
+            throwUserNotFoundError(_userId);
+        }
+        logger.debug('Cognito response: ', { cognitoResponse });
+        const cognitoUser: CognitoUser = unpackAdminGetUserResponse(cognitoResponse);
+        return getUserInfoFromCognitoUser(cognitoUser);
     }
 
     async getUserById(_adminId: string, _userId: string): Promise<UserInfo | null> {
-        throw new Error('Not implemented');
+        logger.debug('Getting user by id: ', { _adminId, _userId });
+        const cognitoResponse = await cognitoApiClient.getUserDetails(_userId);
+        if (!cognitoResponse) {
+            throwUserNotFoundError(_userId);
+        }
+        logger.debug('Cognito response: ', { cognitoResponse });
+        const cognitoUser: CognitoUser = unpackAdminGetUserResponse(cognitoResponse);
+        return getUserInfoFromCognitoUser(cognitoUser);
     }
 
     async listUsers(_adminId: string, _filters: ListUsersFilters): Promise<ListUsersResult> {
-        throw new Error('Not implemented');
+        logger.debug('Listing users: ', { _adminId, _filters });
+        const cognitoResponse = await cognitoApiClient.listUsers();
+        if (!cognitoResponse) {
+            throw new InternalServerError();
+        }
+        logger.debug('Cognito response: ', { cognitoResponse });
+        const users: CognitoUser[] = unpackListUsersResponse(cognitoResponse);
+        return {
+            data: users.map(user => getUserInfoFromCognitoUser(user)),
+            totalItems: users.length,
+        };
     }
 
-    async updateOwnProfile(_userId: string, _payload: UpdateProfilePayload): Promise<void> {
-        throw new Error('Not implemented');
-    }
-
-    async updateUserProfileAsAdmin(_adminId: string, _userId: string, _payload: UpdateProfilePayload): Promise<void> {
-        throw new Error('Not implemented');
-    }
-
-    async deleteUser(_adminId: string, _userId: string): Promise<void> {
+    async updateUserRole(
+        _adminId: string,
+        _userId: string,
+        _payload: UpdateUserRolePayload
+    ): Promise<void> {
         throw new Error('Not implemented');
     }
 
@@ -63,12 +98,13 @@ export class UsersServiceAwsLocal implements UsersService {
     ): Promise<void> {
         throw new Error('Not implemented');
     }
-
-    async updateUserRole(
-        _adminId: string,
-        _userId: string,
-        _payload: UpdateUserRolePayload
-    ): Promise<void> {
-        throw new Error('Not implemented');
+    updateOwnProfile(userId: string, payload: UpdateProfilePayload): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    updateUserProfileAsAdmin(adminId: string, userId: string, payload: UpdateProfilePayload): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    deleteUser(adminId: string, userId: string): Promise<void> {
+        throw new Error('Method not implemented.');
     }
 }
