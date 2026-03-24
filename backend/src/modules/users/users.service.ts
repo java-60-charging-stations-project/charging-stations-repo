99936cd7bc3cf9a6ccd32cb94a393d163ca0c5f9
srgsuh <srg.suhoverhov@gmail.com>
2026-03-24 13:45:20 +0200

    Remove awslocal from user service factory

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,11 +1,11 @@
 import { env } from "../../config/env";
 import { UsersServiceLambda } from "./users.service.lambda";
-import { UsersServiceAwsLocal } from "./users.service.awslocal";
 import { UsersService } from "./users.service.interface";
+import { UsersServiceLocal } from "./users.service.local";
 
 export function buildUsersService(): UsersService {
     if (env.environment === 'local') {
-        return new UsersServiceAwsLocal();
+        return new UsersServiceLocal();
     }
     return new UsersServiceLambda();
 }  
