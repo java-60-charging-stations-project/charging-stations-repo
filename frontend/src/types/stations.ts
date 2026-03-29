4a5c33e50e80501cc77d7afd2dcf5c6fd2fb25e1
srgsuh <srg.suhoverhov@gmail.com>
2026-03-29 11:45:34 +0300

    Add type StationWithPorts

--- frontend/src/types/stations.ts
+++ frontend/src/types/stations.ts
@@ -1,72 +1,5 @@
 export type StationState = 'INACTIVE' | 'ACTIVE' | 'OUT_OF_SERVICE' | 'DELETED';
 
-export interface RatePlan {
-    currencyCode: string;
-    currencyName: string;
-    peakRate: number;
-    offPeakRate: number; 
-};
-
-export interface Location {
-    latitude: number;
-    longitude: number;
-};
-export interface StationBase {
-    id: string;
-    code: string;
-    name: string;
-    owner: string;
-    city: string;
-    address: string;
-    phone: string | null;
-    email: string | null;
-    siteTechnician: string | null;
-    location?: Location;
-    maxPowerKw: number | null;
-    portsCount: number;
-    state: StationState;
-    ratePlan?: RatePlan;
-    createdAt: string;
-    updatedAt: string;
-};
-
-export interface AdminCreateStationRequest {
-    code: string;
-    name: string;
-    owner: string;
-    city: string;
-    address: string;
-    location: Location;
-    maxPowerKw: number;
-    ratePlan: RatePlan;
-    siteTechnician: string | null;
-    phone: string | null;
-    email: string | null;
-};
-
-export interface AdminCreateStationResponse {
-    stationId: string;
-};
-
-export interface ChangeStationStateRequest {
-    oldState: StationState;
-    newState: StationState;
-    updatedAt: string;
-};
-
-export interface ChangeStationStateResponse {
-    updatedAt: string;
-}
-
-export interface StationsListParams {
-    city?: string;
-    owner?: string;
-    state?: StationState;
-    orderBy?: string;
-    page?: number;
-    pageSize?: number;
-}
-
 // PORTS
 export type PortState = 'DISABLED' | 'FREE' | 'BOOKED' | 'OCCUPIED' | 'ERROR';
 
@@ -79,6 +12,7 @@ export type StationPort = {
     updatedAt: string,
 };
 
+// PORTS CREATE
 export type StationPortCreate = {
     portCode: string,
 };
@@ -93,4 +27,76 @@ export type StationPortsCreateResponse = {
 
 export type StationPortsListResponse = {
     ports: StationPort[],
+};
+
+export type RatePlan ={
+    currencyCode: string;
+    currencyName: string;
+    peakRate: number;
+    offPeakRate: number; 
+};
+
+export type GeoLocation = {
+    latitude: number;
+    longitude: number;
+};
+
+export type StationBase = {
+    id: string;
+    code: string;
+    name: string;
+    owner: string;
+    city: string;
+    address: string;
+    phone: string | null;
+    email: string | null;
+    siteTechnician: string | null;
+    location?: GeoLocation;
+    maxPowerKw: number | null;
+    portsCount: number;
+    state: StationState;
+    ratePlan?: RatePlan;
+    createdAt: string;
+    updatedAt: string;
+};
+
+export type StationWithPorts = StationBase & {
+    ports: StationPort[];
+};
+
+export type AdminCreateStationRequest = {
+    code: string;
+    name: string;
+    owner: string;
+    city: string;
+    address: string;
+    location: GeoLocation;
+    maxPowerKw: number;
+    ratePlan: RatePlan;
+    siteTechnician: string | null;
+    phone: string | null;
+    email: string | null;
+};
+
+export type AdminCreateStationResponse = {
+    stationId: string;
+};
+
+export type ChangeStationStateRequest = {
+    oldState: StationState;
+    newState: StationState;
+    updatedAt: string;
+};
+
+export type ChangeStationStateResponse = {
+    updatedAt: string;
+};
+
+export type StationsListParams = {
+    city?: string;
+    owner?: string;
+    state?: StationState;
+    orderBy?: string;
+    page?: number;
+    pageSize?: number;
 };
