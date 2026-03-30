3bdf2a5a580a25ebb14d1020de40ac50ad11baed
srgsuh <srg.suhoverhov@gmail.com>
2026-03-30 14:25:25 +0300

    Apply a debouncer and add city query parameter to the guest fetches

--- frontend/src/pages/support/SupportDashboardPage.tsx
+++ frontend/src/pages/support/SupportDashboardPage.tsx
@@ -1,6 +1,8 @@
 const SupportDashboardPage = () => {
   return (
-    <div><h1>SUPPORT DASHBOARD PAGE</h1></div>
+    <div>
+      <h1 className="text-2xl font-bold text-center">Technical support dashboard</h1>
+    </div>
   );
 };
 
