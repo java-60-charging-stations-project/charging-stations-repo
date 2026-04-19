7dbb51b2c4d4f7c3c5bd35972d4632d1ad65274f
srgsuh <srg.suhoverhov@gmail.com>
2026-04-19 16:45:58 +0300

    Create logs pages for admin and support

---
+++ frontend/src/pages/admin/AdminLogsPage.tsx
@@ -0,0 +1,12 @@
+import LogsTable from "@/components/LogsTable";
+
+const AdminLogsPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center">Administrator logs</h1>
+      <LogsTable />
+    </div>
+  );
+};
+
+export default AdminLogsPage;
--- frontend/src/pages/support/SupportLogsPage.tsx
+++ frontend/src/pages/support/SupportLogsPage.tsx
@@ -1,7 +1,10 @@
+import LogsTable from "@/components/LogsTable";
+
 const SupportLogsPage = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold text-center">Technical support logs</h1>
+      <LogsTable />
     </div>
   );
 };
